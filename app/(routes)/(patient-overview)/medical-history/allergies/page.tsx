3661256fc36623b9f0b12f51306c2950c9cc82dd
Ree <reejohndave.dignos@hcdc.edu.ph>
2024-03-15 13:46:19 +0800

    with modals

--- app/(routes)/(patient-overview)/medical-history/allergies/page.tsx
+++ app/(routes)/(patient-overview)/medical-history/allergies/page.tsx
@@ -8,6 +8,7 @@ import Edit from "@/components/shared/buttons/view";
 import { useState } from "react";
 import { onNavigate } from "@/actions/navigation";
 import { useRouter } from "next/navigation";
+import { Modal } from "@/components/modals";
 
 const Allergies = () => {
   const router = useRouter();
@@ -19,6 +20,12 @@ const Allergies = () => {
   const optionsOrderedBy = ["Accending", "Decending"];
   const optionsSortBy = ["Type", "Severity", "Reaction", "Notes"];
   // end of orderby & sortby function
+
+  const [isOpen, setIsOpen] = useState(false);
+  
+  const isModalOpen = (isOpen: boolean) => {
+    setIsOpen(isOpen);
+  };
 
   return (
     <div className="  w-full">
@@ -42,8 +49,25 @@ const Allergies = () => {
           </p>
         </div>
         <div className="flex flex-row justify-end">
-          <Add></Add>
-          <DownloadPDF></DownloadPDF>
+          <button
+            onClick={() => isModalOpen(true)}
+            className=" mr-2 btn-add text-[#000000] w-[109px] h-[42px] radiu"
+          >
+            <img
+              src="/imgs/add.svg"
+              alt="Custom Icon"
+              className="w-5 h-5 mr-2"
+            />
+            Add
+          </button>
+          <button className="btn-pdfs hover:bg-[#007C85] h-[42px] hover:border-[#007C85] hover:text-white flex items-center justify-center rounded-lg font-manrope text-black text-lg px-8 py-4 border-2 border-gray-300 text-center w-64 relative ">
+            <img
+              src="/imgs/downloadpdf.svg"
+              alt="Custom Icon"
+              className="w-5 h-5 mr-2"
+            />
+            Download PDF
+          </button>
         </div>
       </div>
 
@@ -251,6 +275,9 @@ const Allergies = () => {
           </div>
         </div>
       </div>  
+      {isOpen && (
+        <Modal isModalOpen={isModalOpen} isOpen={isOpen} label="sample label" />
+      )}
     </div>
   );
 };
