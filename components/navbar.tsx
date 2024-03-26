518962b06cfb55d8321a7c811edd765d01e9bf07
dencio <dencio@gmail.com>
2024-03-26 08:24:44 +0800

    navbar-dropdown

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,14 +4,11 @@ import { onNavigate } from "@/actions/navigation";
 import { FaUser, FaBell } from "react-icons/fa";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
-import DropdownMenuProfile from "./dropdownmenu-profile";
+import NavBarBropdown from "./shared/navbardropdown";
+import { useEffect, useRef, useState } from "react";
 
 export const Navbar = () => {
   const router = useRouter();
-  const [isOpenOrderedBy, setIsOpenOrderedBy] = useState(false);
-
-  const optionsOrderedBy = ["Settings", "Language", "Logout"];
 
   const routes = [
     {
@@ -24,8 +21,26 @@ export const Navbar = () => {
     },
   ];
 
+  const [OpenProfile, setOpenProfile] = useState(false);
+
+  const menuRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+        setOpenProfile(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   return (
-    <div className="fixed bg-[#007C85] w-full  h-[70px] flex items-center justify-between px-[105px]">
+    <div className="fixed bg-[#007C85] w-full h-[70px] flex items-center justify-between px-[105px] z-10">
       <Image src={"/imgs/colina-logo.png"} alt={""} width={200} height={37} />
       <div className="flex gap-[20px] items-center">
         <div className="flex gap-[20px]">
@@ -40,19 +55,17 @@ export const Navbar = () => {
           ))}
         </div>
         <div className="flex gap-3 items-center">
+          <Image src={"/imgs/admin 1.png"} alt={""} width={30} height={30} />
+
           <Image
-            className="rounded-full"
-            src={"/imgs/admin 1.svg"}
+            className="cursor-pointer select-none"
+            onClick={() => setOpenProfile((prev) => !prev)}
+            src={"/svgs/arrow-down.svg"}
             alt={""}
-            width={38}
-            height={38}
+            width={15}
+            height={15}
           />
-          <DropdownMenuProfile
-            options={optionsOrderedBy}
-            open={isOpenOrderedBy}
-            width={"165px"}
-            label={"Select"}
-          />
+          {OpenProfile && <NavBarBropdown ref={menuRef} />}
         </div>
       </div>
     </div>
