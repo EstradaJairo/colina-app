b9b5420f9fecd8076cfe00bfc622bd112f04d88c
Reejohndave <reejohndave.dignos@hcdc.edu.ph>
2024-03-26 17:29:17 +0800

    sad

--- components/shared/surgeriesmodal.tsx
+++ components/shared/surgeriesmodal.tsx
@@ -16,7 +16,7 @@ export const Modal = ({ label, isOpen, isModalOpen }: Modalprops) => {
     
 
     <div className={`absolute inset-[-100px] bg-[#76898A99] flex items-center justify-center pb-[160px]`}>
-    <div className="w-[676px] h-[550px] bg-[#FFFFFF] rounded-md">
+    <div className="w-[676px] h-[520px] bg-[#FFFFFF] rounded-md">
       <div className="bg-[#ffffff] w-full h-[70px] flex flex-col justify-start rounded-md">
         <h2 className="p-title text-left text-[#071437] pl-9 mt-7">
         Add Medical History Surgeries Log
