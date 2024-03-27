1d0b501a96fd9920c7ca5e6d23fd91b86b97bc6d
jsvllnv <jaszmeineabigaille.villanueva@hcdc.edu.ph>
2024-03-27 10:12:20 +0800

    login updates with input field height, and forgot password and reset password components are added.

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,11 +1,14 @@
 "use client";
 
 import { Login } from "@/components/login";
+import { ForgotPassword } from "@/components/shared/forgotpassword";
+import { ResetPassword } from "@/components/shared/resetpassword";
 
 export default function LoginPage() {
   return (
     <div>
       <Login/>
+
     </div>
   );
 }
