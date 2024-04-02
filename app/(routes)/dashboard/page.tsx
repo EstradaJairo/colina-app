1d75c218f06f9575eb52a42246066593bbdd34f9
Reejohndave <reejohndave.dignos@hcdc.edu.ph>
2024-04-02 09:39:36 +0800

    sad

--- app/(routes)/dashboard/page.tsx
+++ app/(routes)/dashboard/page.tsx
@@ -5,6 +5,7 @@ import React from "react";
 
 const Dashboard = () => {
   const router = useRouter();
+
   return (
     <div className="mt-20 w-full justify-center items-center  mx-28">
       <div>
@@ -15,8 +16,7 @@ const Dashboard = () => {
             here's what's happening with your clinic today!
           </p>
         </div>
-        {/* {/ {/ Start of content /} /} */}
-        <div className=" flex flex-row gap-5 h-[162px]">
+        <div className=" flex flex-row gap-6 h-[162px] mb-6">
           <div className=" w-full rounded-xl border-[1px] border-[#E4E4E7] hover:border-[#007C85]">
             <div className="px-5">
               <div className="text-[#71717A] font-medium text-[20px] mt-5 mb-10 select-none">
@@ -57,130 +57,89 @@ const Dashboard = () => {
             </div>
           </div>
         </div>
-        {/* {/ {/ end of content /} /} */}
+        
         <div className="flex w-full justify-between gap-3 my-3">
-          <div className="flex flex-col w-[1000px] gap-3">
-            {/* {/ {/ Start of Appointments /} /} */}
-            <div className="rounded-t-lg border-[1px] border-gray-200 my-2 w-[1060px] h-[265px]">
+          <div className="flex flex-col w-[1040px] gap-3">
+            
+            <div className="  rounded-xl border-[1px] border-[#E4E4E7] mb-4">
               <div className="mt-2">
-                <div className="font-bold text-[20px] px-5 mt-5 mb-2">
+                <div className="font-bold text-[20px] px-5 select-none">
                   Upcoming Appointments
                 </div>
-                <div className="px-5">Total of 3 Appointments</div>
-                <div className="flex flex-col h-[190px] border-b-1 border-b-gray-950">
-                  <th className="flex justify-between items-center h-[60px] border-b-[1px]">
-                    <td className="px-6 py-4">
-                      <div className="bg-[#FEF9C3] text-[#713F12] font-medium text-[14px] me-2 px-2.5 py-0.5 rounded-full flex items-center">
-                        <img
-                          className="mr-1"
-                          src="/imgs/Ellipse.svg"
-                          alt=""
-                          width={10}
-                          height={10}
-                        />
+                <div className="px-5 select-none">Total of 3 Appointments</div>
+                <div className="flex flex-col h-auto pb-2 border-b-1 border-b-gray-950">
+                  <div className="flex justify-evenly items-center h-[60px]">
+                    <div className="px-6 py-4">
+                      <p className="bg-[#FEF9C3] text-[#713F12] font-medium text-[14px] me-2 px-2.5 py-0.5 rounded-full flex items-center">
+                        <span className="pr-1 text-[#FACC15]">●</span>
                         Pending
-                      </div>
-                    </td>
-                    <td className="px-6 py-4 font-bold text-[18px] items-center justify-center">
+                      </p>
+                    </div>
+                    <div className="px-6 py-4 font-bold text-[18px] items-center justify-center">
                       Drake Ramos
-                    </td>
-                    <td className="px-6 py-4 ">
-                      <div>March 26, 2024</div>
-                      <div className="text-gray-400">8:00 - 9:00 PM</div>
-                    </td>
-                    <td className="px-6 py-4 font-medium text-[#71717A]">
-                      New Patient
-                    </td>
-                    <td className="px-6 py-4 cursor-pointer">
-                      <img
-                        className="select-none"
-                        src="/icons/dotshorizontal.svg"
-                        alt=""
-                      />
-                    </td>
-                  </th>
-                  <th className="flex justify-between items-center h-[60px] border-b-[1px]">
-                    <td className="px-6 py-4">
-                      <div className="bg-[#FEF9C3] text-[#713F12] font-medium text-[14px] me-2 px-2.5 py-0.5 rounded-full flex items-center">
-                        <img
-                          className="mr-1"
-                          src="/imgs/Ellipse.svg"
-                          alt=""
-                          width={10}
-                          height={10}
-                        />
+                    </div>
+                    <div className="px-6 py-4 ">
+                      <p>March 26, 2024</p>
+                      <p className="text-gray-400">8:00 - 9:00 PM</p>
+                    </div>
+                    <div className="px-6 py-4">New Patient</div>
+                    <div className="px-6 py-4 cursor-pointer">●●●</div>
+                  </div>
+                  <div className="flex justify-evenly items-center h-[60px]">
+                    <div className="px-6 py-4">
+                      <p className="bg-[#FEF9C3] text-[#713F12] font-medium text-[14px] me-2 px-2.5 py-0.5 rounded-full flex items-center">
+                        <span className="pr-1 text-[#FACC15]">●</span>
                         Pending
-                      </div>
-                    </td>
-                    <td className="px-6 py-4 font-bold text-[18px] items-center justify-center">
+                      </p>
+                    </div>
+                    <div className="px-6 py-4 font-bold text-[18px] items-center justify-center">
                       Jenny Wilson
-                    </td>
-                    <td className="px-6 py-4 ">
-                      <div>March 26, 2024</div>
-                      <div className="text-gray-400">8:00 - 9:00 PM</div>
-                    </td>
-                    <td className="px-6 py-4 font-medium text-[#71717A]">
-                      New Patient
-                    </td>
-                    <td className="px-6 py-4 cursor-pointer">
-                      <img
-                        className="select-none"
-                        src="/icons/dotshorizontal.svg"
-                        alt=""
-                      />
-                    </td>
-                  </th>
-                  <th className="flex justify-between items-center h-[60px]">
-                    <td className="px-6">
-                      <div className="bg-[#FEF9C3] text-[#713F12] font-medium text-[14px] me-2 px-2.5 py-0.5 rounded-full flex items-center">
-                        <img
-                          className="mr-1"
-                          src="/imgs/Ellipse.svg"
-                          alt=""
-                          width={10}
-                          height={10}
-                        />
+                    </div>
+                    <div className="px-6 py-4 ">
+                      <p>March 26, 2024</p>
+                      <p className="text-gray-400">8:00 - 9:00 PM</p>
+                    </div>
+                    <div className="px-6 py-4">New Patient</div>
+                    <div className="px-6 py-4 cursor-pointer">●●●</div>
+                  </div>
+                  <div className="flex justify-evenly items-center h-[60px]">
+                    <div className="px-6 py-4">
+                      <p className="bg-[#FEF9C3] text-[#713F12] font-medium text-[14px] me-2 px-2.5 py-0.5 rounded-full flex items-center">
+                        <span className="pr-1 text-[#FACC15]">●</span>
                         Pending
-                      </div>
-                    </td>
-                    <td className="px-6 py-4 font-bold text-[18px] items-center justify-center">
+                      </p>
+                    </div>
+                    <div className="px-6 py-4 font-bold text-[18px] items-center justify-center">
                       Guy Hawkins
-                    </td>
-                    <td className="px-6 py-4 ">
-                      <div>March 26, 2024</div>
-                      <div className="text-gray-400">8:00 - 9:00 PM</div>
-                    </td>
-                    <td className="px-6 py-4 font-medium text-[#71717A]">
-                      New Patient
-                    </td>
-                    <td className="px-6 py-4 cursor-pointer">
-                      <img
-                        className="select-none"
-                        src="/icons/dotshorizontal.svg"
-                        alt=""
-                      />
-                    </td>
-                  </th>
+                    </div>
+                    <div className="px-6 py-4 ">
+                      <p>March 26, 2024</p>
+                      <p className="text-gray-400">8:00 - 9:00 PM</p>
+                    </div>
+                    <div className="px-6 py-4">New Patient</div>
+                    <div className="px-6 py-4 cursor-pointer">●●●</div>
+                  </div>
                 </div>
               </div>
             </div>
             {/* {/ {/ End of Appointments /} /}
+           
             {/ {/ Start of Average Patient List /} /} */}
-            <div className=" border-[1px] border-[#E4E4E7] rounded-xl flex w-[1060px] h-[285px]">
+            <div className=" border-[1px] border-[#E4E4E7] rounded-xl w-[1040px] h-[305px] select-none">
               <div className="font-bold text-[20px] px-5 h-[277px] mt-5 mb-2">
                 Monthly Patient List
               </div>
             </div>
-            {/* {/ {/ End of Average Patient List /} /} */}
+            
           </div>
-          {/* {/ {/ Start of Due Medicaitons /} /} */}
-          <div className="w-[600px] rounded-xl border-[1px] border-[#E4E4E7] h-[570px] my-2 select-none parent">
-            <div className="font-bold text-[20px] px-5 mt-5 mb-2">
+       
+          <div className="w-[630px] bg-white rounded-xl py-5 px-2 border-[1px] border-[#E4E4E7] select-none">
+            <div className="font-bold text-[20px] px-5 mb-3">
               Due Medications
             </div>
-            <div className="px-5 mb-10 text-[#71717A]">Total of 10 due</div>
+            <div className="px-5 mb-10">Total of 10 Due</div>
             <div className="w-full items-center">
-              <div className=" w-full  flex flex-row my-7">
+              <div className=" w-full  flex flex-row mb-8">
                 <div className="mx-5 pt-1">
                   <img src="/imgs/tao1.svg" alt="" width={48} height={48} />
                 </div>
@@ -197,7 +156,7 @@ const Dashboard = () => {
                   </div>
                 </div>
               </div>
-              <div className=" w-full  flex flex-row my-7">
+              <div className=" w-full  flex flex-row mb-8">
                 <div className="mx-5 pt-1">
                   <img src="/imgs/tao2.svg" alt="" width={48} height={48} />
                 </div>
@@ -214,7 +173,7 @@ const Dashboard = () => {
                   </div>
                 </div>
               </div>
-              <div className=" w-full  flex flex-row my-7">
+              <div className=" w-full  flex flex-row mb-8">
                 <div className="mx-5 pt-1">
                   <img src="/imgs/tao3.svg" alt="" width={48} height={48} />
                 </div>
@@ -231,7 +190,7 @@ const Dashboard = () => {
                   </div>
                 </div>
               </div>
-              <div className=" w-full  flex flex-row mb-5">
+              <div className=" w-full  flex flex-row mb-8">
                 <div className="mx-5 pt-1">
                   <img src="/imgs/tao4.svg" alt="" width={48} height={48} />
                 </div>
@@ -248,13 +207,13 @@ const Dashboard = () => {
                   </div>
                 </div>
               </div>
-              <div className=" w-full  flex flex-row mb-[60px]">
+              <div className=" w-full  flex flex-row mb-10">
                 <div className="mx-5 pt-1">
-                  <img src="/imgs/tao1.svg" alt="" width={48} height={48} />
+                  <img src="/imgs/tao4.svg" alt="" width={48} height={48} />
                 </div>
                 <div className="flex w-full justify-between">
                   <div>
-                    <h1 className="font-bold">Painitan ni Owa</h1>
+                    <h1 className="font-bold">Dennis Albite</h1>
                     <p className="font-normal text-[#71717A]">
                       Bisphosphonates
                     </p>
@@ -265,12 +224,11 @@ const Dashboard = () => {
                   </div>
                 </div>
               </div>
-
               <div className="flex text-[#71717A] font-bold mx-5">
-                <p className="cursor-pointer select-none ">SEE ALL DUE</p>
+                <p className="cursor-pointer ">SEE ALL DUE</p>
                 <span className="mx-3 items-center pt-[3px]">
                   <img
-                    className="cursor-pointer select-none"
+                    className="cursor-pointer"
                     src="/imgs/cheveron-right.svg"
                     alt=""
                     width={20}
@@ -280,8 +238,6 @@ const Dashboard = () => {
               </div>
             </div>
           </div>
-
-          {/* {/ {/ End of Due Medicaitons /} /} */}
         </div>
       </div>
     </div>
